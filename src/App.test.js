5a446e1bed9c134773b5ffe5be7c1c04c211099a
Mustafa Alroomi <mustafa.alroomi@cronos.be>
2018-07-09 14:52:31 +0200

    add scss

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { f2c } from "./components/helper";
-
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
